5abbcde5b1be3e65b324a3a28038d0c8f4bb7741
Camilo Rodriguez Cuaran <camiepisode@outlook.com>
2025-04-13 11:19:43 -0500

    Feat: Upgraded SDK Core

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Public API from @vectry/js-core
+export * from '@vectry/js-core';
+
+// Node-specific implementations
 export { Vectry } from './sdk/Vectry';
 export { HttpTransport } from './transport/HttpTransport';
 export { DefaultContextProvider } from './context/DefaultContextProvider';
